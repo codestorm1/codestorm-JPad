c180a162a36f9f2630d4eb4344ca38190741a015
Bryan Green <twistedogre@gmail.com>
2011-07-04 16:25:04 -0700

    Initial Commit

---
+++ JPadTests/JPadTests.h
@@ -0,0 +1,17 @@
+//
+//  JPadTests.h
+//  JPadTests
+//
+//  Created by Bryan Green on 7/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface JPadTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ JPadTests/JPadTests.m
@@ -0,0 +1,33 @@
+//
+//  JPadTests.m
+//  JPadTests
+//
+//  Created by Bryan Green on 7/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "JPadTests.h"
+
+
+@implementation JPadTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JPadTests");
+}
+
+@end
